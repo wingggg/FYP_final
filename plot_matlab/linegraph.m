b9c2718be17a48b5da28bdf289aef5e575ba0059
unknown <wsn12@eews506a-022.ic.ac.uk>
2016-06-14 23:59:08 +0100

    matlab plots

---
+++ plot_matlab/linegraph.m
@@ -0,0 +1,18 @@
+% load('threshold_results');
+
+% figure
+% plot(threshold,validAcc,threshold,regAcc);
+% title('Prediction Accuracy Plots Against Threshold For Full Dataset');
+% xlabel('Threshold');
+% ylabel('Accuracy');
+% legend('Valid','Regions');
+
+
+% load('resize_results');
+
+% figure
+% plot(mainTime,validAccuracy,mainTime,regionsAccuracy);
+% title('Prediction Accuracy Plots Against Execution Time for Subset K01');
+% xlabel('Execution Time');
+% ylabel('Accuracy');
+% legend('Valid','Regions');
