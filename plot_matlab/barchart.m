fad46b550b58b41676526bc5de9e9744b5a918c6
unknown <wsn12@eews506a-022.ic.ac.uk>
2016-06-14 14:09:20 +0100

    more matlab stuff

--- plot_matlab/barchart.m
+++ plot_matlab/barchart.m
@@ -2,26 +2,30 @@ load('runtime_results');
 
 %% single variable barchart
 % figure
-% bar(num_vector, runtime_all_pc);
-% title('Execution Time on PC and Raspberry Pi')
+% b = bar(num_vector, mem_all_before_opt);
+% axis([0.5,33.5,0,30]);    % x-axis min, max; y-axis min, max; (no limit:=inf)
+% title('Percentage Memory Usage During Runtime')
 % xlabel('Data Subsets');
-% ylabel('Execution Time (seconds)');
+% ylabel('MEM %');
+% b.FaceColor = 'b';
+% b.EdgeColor = 'w';
 
 %% two variable barchart
-% vector = zeros(33,2);
-% vector(:,1) = runtime_all_before_opt(:);
-% vector(:,2) = runtime_all_threading(:);
-% 
-% figure
-% b = bar(vector);
-% title('Execution Time on Raspberry Pi Before and After Optimisation')
-% xlabel('Data Subsets');
-% ylabel('Execution Time (seconds)');
-% b(1).FaceColor = 'c';
-% b(2).FaceColor = 'g';
-% b(1).EdgeColor = 'w';
-% b(2).EdgeColor = 'w';
-% l = legend('Before optimisation','Multi-threading');
+vector = zeros(33,2);
+vector(:,1) = mem_all_before_opt(:);
+vector(:,2) = mem_all_parallel(:);
+
+figure
+b = bar(vector);
+axis([0.5,33.5,0,35]);    % x-axis min, max; y-axis min, max; (no limit:=inf)
+title('Percentage Memory Usage During Runtime On Pi Before and After Optimisation')
+xlabel('Data Subsets');
+ylabel('MEM %');
+b(1).FaceColor = 'y';
+b(2).FaceColor = 'b';
+b(1).EdgeColor = 'w';
+b(2).EdgeColor = 'w';
+l = legend('Before optimisation','Parallelisation');
 
 %% multi variable stacked
 % vector = zeros(4,33);
